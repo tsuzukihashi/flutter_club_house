fc59e853c78a76bfd68c97404bb2f9bebd33b1ad
tsuzukihashi <tsudukihashi0817@gmail.com>
2021-04-25 04:16:42 +0900

    upcomming rooms

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,52 +7,56 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(
-            CupertinoIcons.search,
-            size: 28.0,
-          ),
-          onPressed: () {},
-        ),
-        actions: [
-          IconButton(
+        appBar: AppBar(
+          elevation: 0,
+          leading: IconButton(
             icon: const Icon(
-              CupertinoIcons.envelope_open,
-              size: 26.0,
-            ),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(
-              CupertinoIcons.calendar,
+              CupertinoIcons.search,
               size: 28.0,
             ),
             onPressed: () {},
           ),
-          IconButton(
-            icon: const Icon(
-              CupertinoIcons.bell,
-              size: 28.0,
+          actions: [
+            IconButton(
+              icon: const Icon(
+                CupertinoIcons.envelope_open,
+                size: 26.0,
+              ),
+              onPressed: () {},
             ),
-            onPressed: () {},
-          ),
-          GestureDetector(
-            onTap: () {},
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(8.0, 10.0, 20.0, 10.0),
-              child: UserProfileImage(
-                imageUrl: currentUser.imageUrl,
-                size: 36.0,
+            IconButton(
+              icon: const Icon(
+                CupertinoIcons.calendar,
+                size: 28.0,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(
+                CupertinoIcons.bell,
+                size: 28.0,
+              ),
+              onPressed: () {},
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 10.0, 20.0, 10.0),
+                child: UserProfileImage(
+                  imageUrl: currentUser.imageUrl,
+                  size: 36.0,
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-      body: Center(
-        child: UserProfileImage(imageUrl: currentUser.imageUrl),
-      ),
-    );
+          ],
+        ),
+        body: ListView(
+          padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 120.0),
+          children: [
+            UpcomingRooms(
+              upcomingRooms: upcomingRoomsList,
+            )
+          ],
+        ));
   }
 }
