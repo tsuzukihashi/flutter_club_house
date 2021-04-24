e2775bdbea01561d428c7252313ec6cba196be96
tsuzukihashi <tsudukihashi0817@gmail.com>
2021-04-25 01:50:55 +0900

    トップを作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_club_house/screens/home_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'config/pallet.dart';
 
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         fontFamily: GoogleFonts.montserrat().fontFamily,
         textTheme: GoogleFonts.montserratTextTheme(),
       ),
-      home: Scaffold(),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_club_house/entity/data.dart';
+import 'package:flutter_club_house/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(
+            CupertinoIcons.search,
+            size: 28.0,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              CupertinoIcons.envelope_open,
+              size: 26.0,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(
+              CupertinoIcons.calendar,
+              size: 28.0,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(
+              CupertinoIcons.bell,
+              size: 28.0,
+            ),
+            onPressed: () {},
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 10.0, 20.0, 10.0),
+              child: UserProfileImage(
+                imageUrl: currentUser.imageUrl,
+                size: 36.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: UserProfileImage(imageUrl: currentUser.imageUrl),
+      ),
+    );
+  }
+}
